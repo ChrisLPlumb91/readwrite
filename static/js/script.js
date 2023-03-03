404b68bd1490ad9e3a5bf677af34ffd37489b218
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-03-03 21:48:46 +0000

    Add refinements to JavaScript and CSS for fixed login status/new bulletin button/rules

--- static/js/script.js
+++ static/js/script.js
@@ -1,54 +1,71 @@
 $(document).scroll(function() {
-    var y = $(document).scrollTop();  
-    var rulesCard = $('.rules');
-    var newBulletinButton = $('.new-bulletin-button-container');
-    var loggedInAs = $('.logged-in-as')
     var body = $('body');
-    var scrollTopHeightLimit = $(document).outerHeight(true) - ($('#bulletin-card-1').outerHeight(true) * 4 + $('footer').outerHeight(true) + 170);
+    var loggedInAs = $('.logged-in-as');
+    var newBulletinButton = $('.new-bulletin-button-container');
+    var rulesCard = $('.rules');
     
-    if(y > 195 && y <= scrollTopHeightLimit)  {
-        body.removeClass('position-relative');
-        rulesCard.removeClass('rules-fixed-bottom');
-        newBulletinButton.removeClass('button-fixed-bottom');
-        loggedInAs.removeClass('logged-in-as-fixed-bottom');
+    var sidePanel = $(loggedInAs).outerHeight(true) + $(newBulletinButton).outerHeight(true) + $(rulesCard).outerHeight(true);
+    var sidePanelAlt = $(newBulletinButton).outerHeight(true) + $(rulesCard).outerHeight(true);
+    var childrenOfSidePanel = $('#side-panel').children();
+    var y = $(document).scrollTop() + sidePanel;
+    var yAlt = $(document).scrollTop() + sidePanelAlt; 
+    var navBarHeight = $('#navbar').outerHeight(true) + 30; 
+    
+    var lastChild = $('#content-container-base').children().last();
+    var lastChildPosition = lastChild.position().top;
+    var lastChildHeightWithoutBottomMargin = lastChild.innerHeight() + parseInt(lastChild.css('margin-top'));
+    var lastChildPositionBottom = lastChildPosition + lastChildHeightWithoutBottomMargin;
 
-        rulesCard.addClass('rules-fixed');
-        newBulletinButton.addClass('button-fixed');
-        loggedInAs.addClass('logged-in-as-fixed');
-        document.addClass('me-2');
-    } else if(y > scrollTopHeightLimit) {
-        rulesCard.removeClass('rules-fixed');
-        newBulletinButton.removeClass('button-fixed');
-        loggedInAs.removeClass('logged-in-as-fixed');
-
-        body.addClass('position-relative');
-        loggedInAs.addClass('logged-in-as-fixed-bottom');
-        newBulletinButton.addClass('button-fixed-bottom');
-        rulesCard.addClass('rules-fixed-bottom');
+    if(childrenOfSidePanel[0].getAttribute('class').includes('text-center logged-in-as')) {
+        if((y - sidePanel) > navBarHeight && y < lastChildPositionBottom) {
+            body.removeClass('position-relative');
+            loggedInAs.removeClass('logged-in-as-fixed-bottom');
+            newBulletinButton.removeClass('button-fixed-bottom');
+            rulesCard.removeClass('rules-fixed-bottom');
+                
+            loggedInAs.addClass('logged-in-as-fixed');
+            newBulletinButton.addClass('button-fixed');
+            rulesCard.addClass('rules-fixed');
+                
+            document.addClass('me-2');
+        } else if(y >= lastChildPositionBottom) {
+            loggedInAs.removeClass('logged-in-as-fixed');
+            newBulletinButton.removeClass('button-fixed');
+            rulesCard.removeClass('rules-fixed');
+            
+            body.addClass('position-relative');
+            loggedInAs.addClass('logged-in-as-fixed-bottom');
+            newBulletinButton.addClass('button-fixed-bottom');
+            rulesCard.addClass('rules-fixed-bottom');
+        } else {
+            loggedInAs.removeClass('logged-in-as-fixed');
+            newBulletinButton.removeClass('button-fixed');
+            rulesCard.removeClass('rules-fixed');
+                
+            document.removeClass('me-2');
+        }
     } else {
-        rulesCard.removeClass('rules-fixed');
-        newBulletinButton.removeClass('button-fixed');
-        loggedInAs.removeClass('logged-in-as-fixed');
-        document.removeClass('me-2');
-    }      
+        console.log('not logged in');
+        if((yAlt - sidePanelAlt) > navBarHeight && yAlt < lastChildPositionBottom) {
+            console.log('fixing side panel');
+            body.removeClass('position-relative');
+            rulesCard.removeClass('rules-fixed-bottom-alt');
+            newBulletinButton.removeClass('button-fixed-bottom-alt');
+        
+            rulesCard.addClass('rules-fixed-alt');
+            newBulletinButton.addClass('button-fixed-alt');
+            document.addClass('me-2');
+        } else if(yAlt >= lastChildPositionBottom) {
+            rulesCard.removeClass('rules-fixed-alt');
+            newBulletinButton.removeClass('button-fixed-alt');
+        
+            body.addClass('position-relative');
+            newBulletinButton.addClass('button-fixed-bottom-alt');
+            rulesCard.addClass('rules-fixed-bottom-alt');
+        } else {
+            rulesCard.removeClass('rules-fixed-alt');
+            newBulletinButton.removeClass('button-fixed-alt');
+            document.removeClass('me-2');
+        }
+    }
 });
-
-// let form = $('#comment-like-form')
-
-// $('button').one('mouseup', function(event) {
-//     console.log('Liking/Unliking...')
-//     form = $(this).parent();
-//     form.submit();    
-//     event.preventDefault();
-// })
-
-// $('form').on('submit', function(e) {
-//     console.log('Submitting form...');
-// })
-
-// $('#comment-liked').one('click', function(event) {
-//     console.log('Submitting form and unliking...');
-//     form.submit();
-// })
-
-
